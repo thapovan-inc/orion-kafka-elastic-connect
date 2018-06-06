9d690aa5cfc282c762baf1425588084fc308b8a5
kumar <ksubramanian@thapovan-inc.com>
2018-06-06 21:44:33 +0530

    kafka summary json updated with additional data

--- src/main/java/com/thapovan/kafka/KafkaESSummaryConsumer.java
+++ src/main/java/com/thapovan/kafka/KafkaESSummaryConsumer.java
@@ -13,12 +13,14 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 public class KafkaESSummaryConsumer implements Runnable
@@ -87,7 +89,9 @@ public class KafkaESSummaryConsumer implements Runnable
             {
                 try
                 {
-                    Thread.sleep(500); int tmpCounter = 0;
+                    Thread.sleep(500);
+
+                    int tmpCounter = 0;
 
                     ConsumerRecords<String, String> records = consumer.poll(100);
 
@@ -99,10 +103,41 @@ public class KafkaESSummaryConsumer implements Runnable
 
                         String value = record.value();
 
-                        LOG.info("key: {}, value: {}", key, value);
+                        JSONObject data = new JSONObject(value);
+
+                        List<Object> list = data.getJSONArray("serviceNames").toList();
+
+                        StringBuilder sb = new StringBuilder();
+
+                        list.forEach(o -> sb.append((String) o).append(" "));
+
+                        data.put("serviceNamesStr", sb.toString());
+
+                        long diff = (data.getInt("endTime") - data.getInt("startTime")) / 1000;
+
+                        data.put("duration", (diff / 1000));
+
+                        String status = "PASS";
+
+                        JSONObject traceEventSummary = data.getJSONObject("traceEventSummary");
+
+                        data.put("traceEventSummary", traceEventSummary);
+
+                        if (traceEventSummary.getInt("ERROR") > 0 || traceEventSummary.getInt("CRITICAL") > 0)
+                        {
+                            status = "FAIL";
+                        }
+                        else if (diff > 4000)
+                        {
+                            status = "SLOW";
+                        }
+
+                        data.put("status", status);
+
+                        LOG.info("key: {}, value: {}", key, data.toString());
 
                         bulkRequest.add(client.prepareIndex("summaries", "su", key)
-                                .setSource(value, XContentType.JSON)).get();
+                                .setSource(data.toString(), XContentType.JSON)).get();
 
                         tmpCounter++;
                     }
